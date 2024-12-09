39e3a8c398463ac612438ffc1c37b82698fe8c33
Mohammed Faizan <64662114+faizan10045@users.noreply.github.com>
2024-12-09 18:11:10 -0500

    Update add_cron_jobs.sh

--- add_cron_jobs.sh
+++ add_cron_jobs.sh
@@ -2,10 +2,10 @@
 
 # Define the cron lines with comments
 cron_line1="# At midnight daily"
-cron_line2="0 0 * * *  ~/mtp_routine/main.sh >/dev/null 2>&1"
+cron_line2="0 0 * * *  ~/routine/main.sh >/dev/null 2>&1"
 
 cron_line3="# At 1 AM daily"
-cron_line4="0 1 * * * sleep 604800 && ~/mtp_routine/clear.sh >/dev/null 2>&1"
+cron_line4="0 1 * * * sleep 604800 && ~/routine/clear.sh >/dev/null 2>&1"
 
 # Add the cron lines to the crontab
 (crontab -l 2>/dev/null; echo "$cron_line1"; echo "$cron_line2") | crontab -
